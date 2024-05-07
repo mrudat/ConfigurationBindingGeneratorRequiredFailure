325c2e568fdc650fe3092500147168d0c3b1a889
Martin Rudat <mrudat@toraboka.com>
2024-05-07 22:50:20 +1000

    Add project files.

---
+++ ConfigurationBindingGeneratorRequiredFailure/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConfigurationBindingGeneratorRequiredFailure;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+        builder.Configuration
+            .AddInMemoryCollection(new Dictionary<string, string?>()
+            {
+                //["config:ChildOptionsWithRequiredField:RequiredField"] = "value"
+                ["config:Something"] = "value"
+            });
+
+        builder.Services
+            .AddSingleton<IValidateOptions<OptionsWithChildWithRequiredField>, ValidateOptionsWithRequiredField>();
+
+        builder.Services
+            .Configure<OptionsWithChildWithRequiredField>(builder.Configuration.GetRequiredSection(OptionsWithChildWithRequiredField.ConfigurationSectionName));
+
+        var host = builder.Build();
+
+        host.Run();
+    }
+}
+
+public sealed record OptionsWithChildWithRequiredField
+{
+    public const string ConfigurationSectionName = "config";
+
+    [Required]
+    [ValidateObjectMembers]
+    public required ChildOptionsWithRequiredField ChildOptionsWithRequiredField { get; set; }
+}
+
+public sealed record ChildOptionsWithRequiredField
+{
+    [Required]
+    public /*required*/ string RequiredField { get; set; }
+}
+
+[OptionsValidator]
+public partial class ValidateOptionsWithRequiredField : IValidateOptions<OptionsWithChildWithRequiredField>
+{
+}
